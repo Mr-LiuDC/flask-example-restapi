1d23345f1a3d2fc2ee7a598f1c8979d1fe14269d
Leo <442823529@qq.com>
2024-12-20 15:44:35 +0800

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,2 @@
 SECRET_KEY = 'my_secret_key'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db/flask.sqlite3'
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:followup2019@dev.shukangyun.com:53314/test_db?charset=utf8'
